cfe7ca8a5649038f93ec08c1f058c345a3c3dd66
MDausmann-cmri <mdausmann@cmri.org.au>
2019-05-27 13:18:36 +1000

    2.1

--- ex2_1.c
+++ ex2_1.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 #include <float.h>
 
-void printBits(long i);
+void printBits(unsigned long i);
 
 int main()
 {
@@ -11,7 +11,7 @@ int main()
   Write a program to determine the ranges of char, short, int,
   and long variables, both signed and unsigned, by printing appropriate values from standard headers and by direct computation. Harder if you com- pute them: determine the ranges of the various floating-point types.
   */
-  printf("Basic Datatype limits\n");
+  printf("Basic Datatype limits (understanding 2s complement)\n");
   printf("\nchar %d -> %d\n", CHAR_MIN, CHAR_MAX);
   printf("CHAR_MAX"); printBits(CHAR_MAX);
   printf("\nshort %d -> %d\n", SHRT_MIN, SHRT_MAX);
@@ -21,17 +21,17 @@ int main()
   printf("\nlong %ld -> %ld\n", LONG_MIN, LONG_MAX);
   printf("LONG_MAX"); printBits(LONG_MAX);
   
-  printf("\nunsigned char -> %d\n", UCHAR_MAX);
+  printf("\nunsigned char -> %u\n", UCHAR_MAX);
   printf("UCHAR_MAX"); printBits(UCHAR_MAX);
-  printf("\nunsigned short -> %d\n", USHRT_MAX);
+  printf("\nunsigned short -> %u\n", USHRT_MAX);
   printf("USHRT_MAX"); printBits(USHRT_MAX);
-  printf("\nunsigned int -> %d\n", UINT_MAX);
+  printf("\nunsigned int -> %u\n", UINT_MAX);
   printf("UINT_MAX"); printBits(UINT_MAX);
-  printf("\nunsigned long -> %ld\n", ULONG_MAX);
+  printf("\nunsigned long -> %lu\n", ULONG_MAX);
   printf("ULONG_MAX"); printBits(ULONG_MAX);
 
   // boundary behaviour
-  printf("Boundary Behaviour\n");
+  printf("\nBoundary Behaviour\n");
   int i; i = INT_MIN;
   printf("i(INT_MIN)=%d\n", i);
   i = i - 1;
@@ -43,26 +43,21 @@ int main()
 
 }
 
-void printBits(long i){
-  unsigned long a, pa;
+void printBits(unsigned long i){
+  unsigned long a;
   int b;
   if(i < 2)
-    printf(" is too low to calculate");
+    printf(" is too low to calculate\n");
   else {
     a = 1;
     for(b=1; b < INT_MAX; ++b){
-      pa = a;
       a = a * 2;
       if(a==0) {
-        printf(" multiplication tapped out at %d bits with a value of %ld\n", b-1, pa);
+        printf(" multiplication tapped out at %d bits \n", b);
         return;
       }
-      if(a == i) {
-        printf(" is a whole binary number with %d bits\n", b);
-        return;
-      }
-      if(a > i){
-        printf(" falls between whole binary numbers between %d and %d bits\n", b, b + 1);
+      if(a-1 == i) {
+        printf(" uses %d bits\n", b);
         return;
       }
     }
