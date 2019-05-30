b89185167c3ca5413cf78ed68949fc5f2b9699ae
Michael Dausmann <mdausmann@gmail.com>
2019-05-30 10:40:47 +1000

    play with bits

--- scratch.c
+++ scratch.c
@@ -10,6 +10,7 @@ enum boolean { YES, NO };
 void printString(char aString[]);
 void intToChar();
 void use_strtol();
+void use_bitwise();
 
 int main()
 {
@@ -30,6 +31,8 @@ int main()
   intToChar();
 
   use_strtol();
+
+  use_bitwise();
 }
 
 void printString(char aString[]){
@@ -61,4 +64,29 @@ void use_strtol(){
   char *ptr;
   long lfromhex = strtol("oxaf", &ptr, 10);
   printf("oxaf -> %ld\n", lfromhex);
+}
+
+void use_bitwise()
+{
+  //   631
+  //   4268421  <- places
+  //   1421421  <- handy dandy octal converter
+  int b0010010 = 022;
+  int b0001010 = 012;
+  int b0000010 = b0010010 & b0001010; //= 02 octal/dec. & (AND) MASKS OUT input bits where the mask is 0 
+  printf("0010010 & 0001010 = 0000010 %d [2]\n", b0000010);
+
+  int b0011010 = b0010010 | b0001010; //= 32 octal 26dec. | (OR) turns bits on 
+  printf("0010010 | 0001010 = 0011010 %d [26]\n", b0011010);
+
+  int b0011000 = b0010010 ^ b0001010; //=030 octal 24 dec. ^ (XOR) gives 1 if difference which is highlighted 
+  printf("0010010 ^ 0001010 = 0011000 %d [24]\n", b0011000);
+
+  // ~ (ones complement) flips bits so x = x & ~077 sets the last 6 bits to zero
+  // so you could have a bitmask with 1's in the values you want to represent
+  // and use & ~MASK to turn the bits in the mask OFF in the lhs
+  int b0010000 = b0010010 & ~07; //=020 octal 16 dec.
+  printf("0010010 ^ ~07  = 0010000 %d [16]\n", b0010000);
+
+
 }
