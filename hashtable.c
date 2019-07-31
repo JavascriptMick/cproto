d740be299c1e58e02339dc8efa6b4efee4e574eb
Michael Dausmann <mdausmann@gmail.com>
2019-07-31 22:50:40 +1000

    Linklist and Hashtable implementation

--- hashtable.c
+++ hashtable.c
@@ -1,11 +1,119 @@
 #include <stdio.h>
-//implement a simple hash table 
-//which maps char to char[]
-#define HASHTABLESIZE 100
-// hmm, needs to b an array of pointers to some structure which includes a char (key) and char[] value
-char hashtable[HASHTABLESIZE];
+#include <stdlib.h> //malloc
+#include <math.h>   //floor
+#include <string.h> //strcmp
 
-int main(){
-  // hashtable = "hello";
+//implement a simple hash table 
+//which stores Int values with a string key
+#define HASHTABLESIZE 15
+#define HASHCODECEILING 1000
+
+/* linked List Bits */
+struct snode {
+  struct snode *next;
+  char *key;
+  int data;
+};
+
+struct snode *salloc(void){
+  return (struct snode *) malloc(sizeof(struct snode));
 }
 
+struct snode *create_snode(int data, char *key){
+  struct snode *new_snode = salloc();
+  new_snode->data = data;
+  new_snode->key = key;
+  new_snode->next = NULL;
+  return new_snode;
+}
+
+void appendsnode(struct snode *node, int data, char *key){
+  if(node->next != NULL)
+    appendsnode(node->next, data, key);
+  else
+    node->next = create_snode(data, key);
+}
+
+int snodefind(struct snode *node, char *key){
+  if(strcmp(node->key, key) == 0)
+    return node->data;
+  else if(node->next != NULL)
+    return(snodefind(node->next, key));
+  else
+    return 0; 
+}
+
+void snodeprint(struct snode *p, int top){
+  if(p != NULL) {
+    if(top==0)
+      printf("->");
+    printf("%s:%d", p->key, p->data);
+    snodeprint(p->next, 0);
+  }
+  if(top!=0)
+    printf("\n");
+}
+
+/* Hashing bits (very simplistic) */
+int hashcode(char *key){
+  char c;
+  int hashcode = 0;
+  while((c = *key++) != '\0'){
+    hashcode += c;
+  }
+  return hashcode;
+}
+
+int map(int hashcode){
+  float hc = (float)hashcode;
+  float c = (float)HASHCODECEILING;
+  if(hashcode > HASHCODECEILING)
+    return HASHTABLESIZE - 1;
+  else
+    return (int)floor((hc/c) * HASHTABLESIZE);
+}
+
+/* Hashtable bits */
+struct snode *hashtable[HASHTABLESIZE];
+
+void put(int data, char *key){
+  int i = map(hashcode(key));
+  if(hashtable[i] == NULL){
+    hashtable[i] = create_snode(data, key);
+  } else {
+    appendsnode(hashtable[i], data, key);
+  }
+}
+
+int get(char *key){
+  int i = map(hashcode(key));
+  if(hashtable[i] == NULL){
+    return 0; //TODO - I guess this should be an exception?
+  } else {
+    return snodefind(hashtable[i], key);
+  }  
+}
+
+void hashtableprint(){
+  for(int i = 0; i < HASHTABLESIZE; i++){
+    printf("%d->", i);
+    if(hashtable[i] == NULL){
+      printf("\n");
+    } else {
+      snodeprint(hashtable[i], 1);
+    }
+  }
+}
+
+int main(){
+  printf("%s -> %d -> %d\n", "a", hashcode("a"), map(hashcode("a")));
+  printf("%s -> %d -> %d\n", "Abignail", hashcode("Abignail"), map(hashcode("Abignail")));
+  printf("%s -> %d -> %d\n", "quitelongnameeee", hashcode("quitelongnameeee"), map(hashcode("quitelongnameeee")));
+  printf("%s -> %d -> %d\n", "realyrealylongnamelonglong", hashcode("realyrealylongnamelonglong"), map(hashcode("realyrealylongnamelonglong")));
+
+  put(356, "Bob");
+  put(444, "quitelongnameeee");
+  put(355556, "realyrealylongnamelonglong");
+  hashtableprint();
+  printf("find Bob = %d\n", get("Bob"));
+}
--- linklist.c
+++ linklist.c
@@ -1,21 +1,65 @@
 #include <stdio.h> /* NULL */
 #include <stdlib.h> /* malloc */
 
-struct node {
-  struct node *next;
+struct inode {
+  struct inode *next;
   int data;
 };
 
-struct node *nalloc(void){
-  return (struct node *) malloc(sizeof(struct node));
+struct snode {
+  struct snode *next;
+  char *key;
+  int data;
+};
+
+struct inode *nalloc(void){
+  return (struct inode *) malloc(sizeof(struct inode));
 }
 
-void nodeprint(struct node *p, int top){
+struct snode *salloc(void){
+  return (struct snode *) malloc(sizeof(struct snode));
+}
+
+struct inode *create_inode(int data){
+  struct inode *new_inode = nalloc();
+  new_inode->data = data;
+  new_inode->next = NULL;
+  return new_inode;
+}
+
+struct snode *create_snode(int data, char *key){
+  struct snode *new_snode = salloc();
+  new_snode->data = data;
+  new_snode->key = key;
+  new_snode->next = NULL;
+  return new_snode;
+}
+
+void append(struct snode *node, int data, char *key){
+  if(node->next != NULL)
+    append(node->next, data, key);
+  else
+    node->next = create_snode(data, key);
+}
+
+
+void inodeprint(struct inode *p, int top){
   if(p != NULL) {
     if(top==0)
       printf("->");
     printf("%d", p->data);
-    nodeprint(p->next, 0);
+    inodeprint(p->next, 0);
+  }
+  if(top!=0)
+    printf("\n");
+}
+
+void snodeprint(struct snode *p, int top){
+  if(p != NULL) {
+    if(top==0)
+      printf("->");
+    printf("%s:%d", p->key, p->data);
+    snodeprint(p->next, 0);
   }
   if(top!=0)
     printf("\n");
@@ -23,18 +67,22 @@ void nodeprint(struct node *p, int top){
 
 int main()
 {
-  struct node n;
+  struct inode n;
   n.data = 42;
-  struct node c;
+  struct inode c;
   c.data = 69;
   n.next = &c;
-  nodeprint(&n, 1);
+  inodeprint(&n, 1);
 
-  struct node *testnext = nalloc();  
+  struct inode *testnext = nalloc();  
   testnext->data = 69;
   testnext->next = NULL;
-  struct node *testnode = nalloc();
-  testnode->data = 42;
-  testnode->next = testnext;
-  nodeprint(testnode, 1);
+  struct inode *testinode = nalloc();
+  testinode->data = 42;
+  testinode->next = testnext;
+  inodeprint(testinode, 1);
+
+  struct snode *new_snode = create_snode(42, "life");
+  append(new_snode, 69, "death");
+  snodeprint(new_snode, 1);
 }
